f3228cd92741d7fa8870cf5cc45bd6f1af0672fd
Juan <jmartinez6784@yahoo.com>
2020-11-16 21:28:05 -0800

    Implemented all CRUD functionality to post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,59 @@
 class PostsController < ApplicationController
+  # before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
-    
+    @post = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    # @post.user_id = current_user.id
+
+    if @post.save
+      flash.notice = "Successfully created a Post!"
+      redirect_to @post
+    else 
+      flash.now[:danger] = "Can't create this Post, there are errors."
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    # @post.user_id = current_user.id
+    @post.update(post_params)
+    flash.notice = "Post edited!"
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash.notice = "Post was deleted!"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption)
+  end
+
+  # def set_post
+  #   if Post.find(params[:id]).user != current_user
+  #     redirect_to posts_path
+  #   end
+  # end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  # belongs_to :user
+  # has_many :comments, dependent: :destroy
+  # has_many :likes, as: :likeable, dependent: :destroy
+
+  validates :caption, presence: true
+  validates :caption, length: { maximum: 250 }
+  # validates :image, presence: true, unless: :body?
+  # has_one_attached :image, dependent: :destroy
+
+  # validate :image_type, unless: ->(x) { x.image.blank? }
+  
+  # private
+
+  # def image_type
+  #   if image.attached? && !image.content_type.in?(%(image/jpeg image/png))
+  #     errors.add(:image, "must be a JPEG or PNG.")
+  #   end
+  # end
 end
