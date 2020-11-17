f2e801c67edd5fabb1564b5a4c855d420dfb45c9
Juan <jmartinez6784@yahoo.com>
2020-11-16 23:19:36 -0800

    Post model associations with user model and modified views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    # @post.user_id = current_user.id
+    @post.user_id = current_user.id
 
     if @post.save
       flash.notice = "Successfully created a Post!"
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @user = User.find(@post.user_id)
   end
 
   def edit
@@ -32,7 +33,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    # @post.user_id = current_user.id
+    @post.user_id = current_user.id
     @post.update(post_params)
     flash.notice = "Post edited!"
     redirect_to post_path(@post)
