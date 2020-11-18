66d0c8864a5d764e5f5f95244dc2ba7a278792a4
Juan <jmartinez6784@yahoo.com>
2020-11-18 14:21:12 -0800

    Implemented likes model with comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,31 @@
 class LikesController < ApplicationController
   def ajax_likes_update
-    @post = Post.find(params[:id])
-    like = Like.where(post_id: @post.id, user_id: current_user.id)
+    if params[:type] === "post"
+      @post = Post.find(params[:id])
+      @like = Like.where(post_id: @post.id, user_id: current_user.id)
 
-    if like == []
-      @post.likes.create!(user_id: current_user.id, post_id: @post.id)
-    else
-      like.destroy_all
+      if @like == []
+        @post.likes.create!(user_id: current_user.id, post_id: @post.id)
+        @like_txt = "UnLike"
+      else
+        @like.destroy_all
+        @like_txt = "Like"
+      end
+
+      @like_count = @post.likes.count.to_s + " " + "Like".pluralize(@post.likes.count)
+    elsif params[:type] === "comment"
+      @comment = Comment.find(params[:id])
+      @like = Like.where(comment_id: @comment.id, user_id: current_user.id)
+
+      if @like == []
+        @comment.likes.create!(user_id: current_user.id, comment_id: @comment.id)
+        @like_txt = "UnLike"
+      else
+        @like.destroy_all
+        @like_txt = "Like"
+      end
+
+      @like_count = @comment.likes.count.to_s + " " + "Like".pluralize(@comment.likes.count)
     end
 
     respond_to do |format|
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,3 @@
 class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true
-
-  validates :user_id, uniqueness: {
-    scope: :post_id
-  }
 end
