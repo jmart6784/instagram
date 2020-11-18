4b75630f7dab2d229ef394f0aee516ae3bcb5dc3
Juan <jmartinez6784@yahoo.com>
2020-11-17 21:10:04 -0800

    Added user index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "posts#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :users, only: [:index, :show]
 
   resources :posts
 
