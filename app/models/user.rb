460fb56c5f07d25289b79c359d94da71420633b0
Juan <jmartinez6784@yahoo.com>
2020-11-17 20:05:51 -0800

    Added first_name, last_name, username, and bio columns to users and integrated them into views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\z/
+  validates :username, uniqueness: true, case_sensitive: false, presence: true, length: {minimum: 4, maximum: 16}, format: { with: VALID_USERNAME_REGEX }
+
+  validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 60 }
+
+  validates :bio, length: { maximum: 1000 }
 end
