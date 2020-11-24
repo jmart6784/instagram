15b09c9b17ecbe1ea50aa8f7d3ad12c815ffd974
Juan <jmartinez6784@yahoo.com>
2020-11-24 12:16:20 -0800

    Added delete to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:destroy]
   before_action do
     @conversation = Conversation.find(params[:conversation_id])
   end
 
   def index
+    session[:return_to] ||= request.referer
+
     @messages = @conversation.messages
 
     if @messages.length > 10
@@ -31,15 +34,29 @@ class MessagesController < ApplicationController
 
   def create
     @message = @conversation.messages.new(message_params)
+    @message.user_id = current_user.id
 
     if @message.save
       redirect_to conversation_messages_path(@conversation)
     end
   end
 
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    flash.notice = "Message was deleted!"
+    redirect_to session.delete(:return_to)
+  end
+
   private
 
   def message_params
-    params.require(:message).permit(:body, :user_id)
+    params.require(:message).permit(:body)
+  end
+
+  def set_message
+    if Message.find(params[:id]). != current_user
+      redirect_to session.delete(:return_to)
+    end
   end
 end
