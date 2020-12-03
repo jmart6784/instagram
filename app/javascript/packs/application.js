324f6317f9b94abd616f4307535f054efb2a0d6e
Juan <jmartinez6784@yahoo.com>
2020-12-02 21:14:48 -0800

    Styled user show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,3 +36,19 @@ document.addEventListener("turbolinks:load", () => {
     }
   });
 });
+
+document.addEventListener("turbolinks:load", () => {
+  let tintedTiles = document.querySelectorAll(".tinted-tile");
+
+  tintedTiles.forEach((tile) => {
+    tile.addEventListener("mouseenter", () => {
+      tile.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+      tile.childNodes[1].style.display = "block";
+    });
+
+    tile.addEventListener("mouseleave", () => {
+      tile.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+      tile.childNodes[1].style.display = "none";
+    });
+  });
+});
