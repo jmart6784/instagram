19749e722c69f4a4a9a185622ca9cee6d5f13f5e
Juan <jmartinez6784@yahoo.com>
2020-12-11 22:05:57 -0800

    Changed root path from post index to following feed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root 'posts#index', as: :authenticated_root
+      root 'posts#following_feed', as: :authenticated_root
     end
   
     unauthenticated do
