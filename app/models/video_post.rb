1f2818ed4a94a4859c56ff2ad713069ac41020ae
Juan <jmartinez6784@yahoo.com>
2020-12-22 21:23:17 -0800

    Styled post form error messages

--- app/models/video_post.rb
+++ app/models/video_post.rb
@@ -6,12 +6,18 @@ class VideoPost < ApplicationRecord
 
   validates :caption, length: { maximum: 2200 }
 
-  validates :clip, presence: true
   has_one_attached :clip, dependent: :destroy
 
+  validate :clip_presence
   validate :clip_type, unless: ->(x) { x.clip.blank? }
   
   private
+
+  def clip_presence
+    unless clip.attached?
+      self.errors[:base] << "Video must be attached"
+    end
+  end
 
   def clip_type
     if clip.attached? && !clip.content_type.in?(%(video/mp4))
