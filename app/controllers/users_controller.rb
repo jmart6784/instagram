7e4548fdc5328bd7dc2b5cbb5bc42cf1d9d343a3
Juan <jmartinez6784@yahoo.com>
2020-12-22 19:56:18 -0800

    improved search logic and added conditional view for no search results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,21 @@ class UsersController < ApplicationController
     if params[:search]
       search_txt = params[:search].downcase
       @users = []
+
+      search_words = search_txt.split(" ")
       
       User.all.select do |user|
-        if user.first_name != ""
-          if user.first_name.downcase[search_txt] || user.last_name.downcase[search_txt] || user.username.downcase[search_txt]
+        next if current_user === user
+
+        search_words.each do |word|
+          if user.first_name.downcase[word] || user.last_name.downcase[word] || user.username.downcase[word]
             @users << user
           end
         end
       end
+
+      @users.uniq!
+
     else
       @users = User.all
     end
