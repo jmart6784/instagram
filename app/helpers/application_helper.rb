5ef45c10f5f24a6304f3c0b3bfd5349baf0fcbab
Juan <jmartinez6784@yahoo.com>
2020-12-25 14:54:54 -0800

    Created method to simplify numbers and used it on user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,19 @@ module ApplicationHelper
       image_tag("default_profile.jpg", width: "#{size}", height: "#{size}")
     end
   end
+
+  def simple_number(num)
+    if num.between?(1000, 999999)
+      num = num + 0.0
+      (num / 1000).round(1).to_s + "K"
+    elsif num.between?(1000000, 999999999)
+      num = num + 0.0
+      (num / 1000000).round(1).to_s + "M"
+    elsif num >= 1000000000
+      num = num + 0.0
+      (num / 1000000000).round(1).to_s + "B"
+    else
+      num
+    end
+  end
 end
