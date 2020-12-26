bc4f6ff9ac72811d2966d144776a9e993e0a050c
Juan <jmartinez6784@yahoo.com>
2020-12-25 20:05:41 -0800

    Changed Message model body length and added show more/less functionality to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
 
-  validates :body, presence: true, length: { maximum: 250 }
+  validates :body, presence: true, length: { maximum: 2200 }
   validates :conversation_id, presence: true
   validates :user_id, presence: true
 end
